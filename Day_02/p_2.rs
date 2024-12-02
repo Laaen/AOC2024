719de7d0c136f0fa36bad1a157e653e01c6dde33
Alex . <Laaen@hotmail.fr>
2024-12-02 18:54:03 +0100

    Fin jour 2

--- Day_02/p_2.rs
+++ Day_02/p_2.rs
@@ -1,48 +1,25 @@
 use std::fs;
+use std::time;
 
-fn check_line(line : &Vec<i32>) -> bool{
+fn check_line(line: &Vec<i32>) -> bool{
+    let sorted = line.is_sorted_by(|a, b| a <= b) || line.is_sorted_by(|a, b| b <= a);
+    let steps_ok = line.windows(2).map(|elt| (elt[0] - elt[1]).abs()).all(|elt| elt <= 3 && elt != 0);
+    return sorted && steps_ok;
+}
 
-    let mut unordered_num = 0;
-    // Number of numbers not in order
-    for elt in line.windows(3){
-        println!("{:?}", elt);
-        if (elt[0] - elt[1]) * (elt[1] - elt[2]) < 0{
-            unordered_num += 1;
+fn check_lines(line : &Vec<i32>) -> bool{
+    for i in 0..(line.len()){
+        if check_line(&[&line[..i], &line[i+1..]].concat()){
+            return true;
         }
     }
-    
-    // Divise par 2, car chaque nombre non ordonné compte pour 2
-    unordered_num /= 2;
-
-    // Errors counter
-    let mut errors = 0;
-
-    for elt in line.windows(3){
-        // If not sorted, add an error
-        if !(elt.is_sorted_by(|a, b| a <= b) || elt.is_sorted_by(|a, b| b <= a)){
-            errors += 1;
-        }
-        if (elt[0] - elt[1]).abs() > 3 || (elt[0] - elt[1]).abs() == 0{
-                errors += 1;
-            if (elt[0] - elt[2]).abs() > 3 || (elt[0] - elt[2]).abs() == 0{
-                errors += 1;
-            }
-        }
-
-    }
-
-    println!("{:?}", line);
-    println!("{:?}", errors);
-
-    return errors <= 1;
+    return false;
 }
 
 fn main(){
     let input = fs::read_to_string("input").expect("Error");
-    let data : Vec<Vec<i32>> = input.split("\n").map(|line| line.split(" ").map(|x| x.parse().unwrap()).collect()).collect();
+    let data : Vec<Vec<i32>> = input.split("\r\n").map(|line| line.split(" ").map(|x| x.parse().unwrap()).collect()).collect();
 
-    let result = data.iter().map(|elt| check_line(elt)).filter(|elt| *elt == true).count();
-
-
+    let result = data.iter().map(|elt| check_lines(elt)).filter(|elt| *elt == true).count();
     println!("{:?}", result);
 }
