e8e591270e8fd21721e96ffae7f9698909a3df02
Alex . <Laaen@hotmail.fr>
2024-12-06 21:25:33 +0100

    Jour 6

--- Day_05/src/main.rs
+++ Day_05/src/main.rs
@@ -1,7 +1,6 @@
 use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::fs;
-use std::time;
 
 fn compare(rules : &HashMap<i32, Vec<i32>>, a : i32, b : i32) -> Ordering{
     // Check if a in hash, if it is return less for each value
@@ -14,48 +13,35 @@ fn compare(rules : &HashMap<i32, Vec<i32>>, a : i32, b : i32) -> Ordering{
     return Ordering::Greater;
 }
 
-fn custom_sort(rules : &HashMap<i32, Vec<i32>>, data : &mut Vec<i32>){
-    data.sort_by(|a, b| compare(rules, *a, *b));
-
-}
-
 fn main() {
 
-    let start = time::Instant::now();
-
     let data = fs::read_to_string("input").unwrap();
-    let cleaned_data = data.replace("\r", "");
-    let tmp: Vec<_> = cleaned_data.split("\n\n").collect();
+    let tmp: Vec<_> = data.split("\r\n\r\n").collect();
 
     let mut rules : HashMap<i32, Vec<i32>> = HashMap::new();
-    for elt in tmp[0].split("\n"){
+    for elt in tmp[0].split("\r\n"){
         let splitted : Vec<i32> = elt.split("|").map(|x| x.parse().unwrap()).collect();
         rules.entry(splitted[0]).and_modify(|x| x.push(splitted[1])).or_insert(vec![splitted[1]]);
     }
 
-    let mut data_to_check : Vec<Vec<i32>> = tmp[1].split("\n")
+    let mut data_to_check : Vec<Vec<i32>> = tmp[1].split("\r\n")
     .map(|elt| elt.split(",").map(|x| x.parse().unwrap()).collect()).collect();
 
-    let mut part_1 : Vec<Vec<i32>> = Vec::new();
-    let mut part_2 : Vec<Vec<i32>> = Vec::new();
+    let mut res_1 = 0;
+    let mut res_2 = 0;
 
-    for elt in data_to_check{
-        let mut copy = elt.clone();
-        custom_sort(&rules, &mut copy);
-        if copy == elt{
-            part_1.push(elt);
+    for elt in data_to_check.iter_mut(){
+
+        if elt.is_sorted_by(|a, b| rules.get(a).unwrap_or(&vec![0]).contains(b)){
+            res_1 += elt[elt.len()/2];
         }
         else{
-            part_2.push(copy);
+            elt.sort_by(|a, b|compare(&rules, *a, *b));
+            res_2 += elt[elt.len()/2];
         }
     }
-
-    let res_1 = part_1.iter().map(|elt| elt[(elt.len()/2)]).fold(0, |res, a| res + a);
-    let res_2 = part_2.iter().map(|elt| elt[(elt.len()/2)]).fold(0, |res, a| res + a);
     
     println!("{:?}", res_1);
     println!("{:?}", res_2);
-
-    println!("Temps écoulé : {:?}", start.elapsed());
 
 }
---
+++ Day_06/src/main.rs
@@ -0,0 +1,115 @@
+use std::{collections::HashSet, fs};
+use std::thread;
+use std::sync::{Arc, Mutex};
+
+fn get_char_at(data : &Vec<String>, x : usize, y: usize) -> char{
+    let line = data.get(x);
+    match line{
+        Some(str) => {str.chars().nth(y).unwrap_or('X')}
+        None => {'X'}
+    }
+}
+
+fn main() {
+
+    let data = fs::read_to_string("input").unwrap();
+    let grid : Arc<Vec<String>> = Arc::new(data.split("\r\n").map(|elt| elt.to_string()).collect());
+
+    // Starting position, used for both parts
+    let start_pos : (usize, usize) = grid.iter()
+    .enumerate()
+    .filter(|(_idx, line)| line.contains("^"))
+    .map(|(idx, line)| (idx, line.find("^").unwrap()))
+    .collect::<Vec<(usize, usize)>>()[0];
+
+    // ======================== Part 1 ========================
+
+    // Get the current position
+    let mut curr_pos = start_pos;
+
+    // Visited positions
+    let mut visited : HashSet<(usize, usize)> = HashSet::new();
+    visited.reserve(10000);
+    visited.insert(curr_pos);
+
+    // Check every direction
+    let directions: Vec<(i32, i32)> = vec![(-1, 0), (0, 1), (1, 0), (0, -1)];
+
+    for d in directions.iter().cycle(){
+        while get_char_at(&grid, (curr_pos.0 as i32 + d.0) as usize, (curr_pos.1 as i32 + d.1) as usize)  != '#' &&
+              get_char_at(&grid, (curr_pos.0 as i32 + d.0) as usize, (curr_pos.1 as i32 + d.1) as usize) != 'X'{
+            curr_pos.0 = (curr_pos.0 as i32 + d.0) as usize;
+            curr_pos.1 = (curr_pos.1 as i32 + d.1) as usize;
+            visited.insert(curr_pos);
+        }
+        if get_char_at(&grid, (curr_pos.0 as i32 + d.0) as usize, (curr_pos.1 as i32 + d.1) as usize) == 'X'{
+            break;
+        }
+    }
+
+    println!("{:?}", visited.len());
+
+    // ======================== Part 2 ========================
+    let result = Arc::new(Mutex::new(0));
+    
+    // handles des threads
+    let mut handles = vec![];
+
+
+    // Bruteforce
+    // Add a # on each position
+    for (i, line) in grid.iter().enumerate(){
+
+        let result_cpy = result.clone();
+        let mut grid_bis : Vec<String> = grid.clone().to_vec();
+        let line_cpy = line.clone();
+
+        let handle = thread::spawn(move || {
+            for(j, chr) in line_cpy.chars().enumerate(){
+                if chr == '.'{
+                    grid_bis[i].replace_range(j..j+1, "#");
+            
+                    // Get the current position
+                    let mut curr_pos = start_pos;
+            
+                    // Backtrace of already visidted coords
+                    let mut visited : HashSet<(usize, usize)> = HashSet::new();
+                    visited.reserve(10000);
+                    let mut nb_identical_pos = 0;
+            
+                    // Check every direction
+                    let directions: Vec<(i32, i32)> = vec![(-1, 0), (0, 1), (1, 0), (0, -1)];
+            
+                    for d in directions.iter().cycle(){
+                        while get_char_at(&grid_bis, (curr_pos.0 as i32 + d.0) as usize, (curr_pos.1 as i32 + d.1) as usize)  != '#' &&
+                            get_char_at(&grid_bis, (curr_pos.0 as i32 + d.0) as usize, (curr_pos.1 as i32 + d.1) as usize) != 'X'{
+                            curr_pos.0 = (curr_pos.0 as i32 + d.0) as usize;
+                            curr_pos.1 = (curr_pos.1 as i32 + d.1) as usize;
+                            if visited.insert(curr_pos) == false{
+                                nb_identical_pos += 1;
+                            }
+                        }
+                        if nb_identical_pos >= 1000{
+                            let mut nb = result_cpy.lock().unwrap();
+                            *nb += 1;
+                            break;
+                        }
+                        if get_char_at(&grid_bis, (curr_pos.0 as i32 + d.0) as usize, (curr_pos.1 as i32 + d.1) as usize) == 'X' {
+                            break;
+                        }
+                    }
+                    //println!("{:?}", grid_bis);
+                    grid_bis[i].replace_range(j..j+1, ".");
+                }
+            }
+        });
+        handles.push(handle);
+    }
+
+    for h in handles{
+        h.join().unwrap();
+    }
+
+    println!("{:?}", result.lock().unwrap());
+}
+    
